086c78694971fcce0925a1c3509a2ef2251007ab
Carlos Zamora <carlos.zamora@microsoft.com>
2021-07-29 08:44:43 -0700

    Implement Keyboard Selection

--- src/cascadia/TerminalSettingsModel/ActionArgs.cpp
+++ src/cascadia/TerminalSettingsModel/ActionArgs.cpp
@@ -34,6 +34,7 @@
 #include "RenameWindowArgs.g.cpp"
 #include "GlobalSummonArgs.g.cpp"
 #include "FocusPaneArgs.g.cpp"
+#include "UpdateSelectionArgs.g.cpp"
 
 #include <LibraryResources.h>
 
@@ -680,5 +681,10 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
             fmt::format(std::wstring_view(RS_(L"FocusPaneCommandKey")),
                         Id())
         };
+    }
+
+    winrt::hstring UpdateSelectionArgs::GenerateName() const
+    {
+        return L"";
     }
 }
